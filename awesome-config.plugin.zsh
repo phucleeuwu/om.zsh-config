93058914bc04685f76c7305ede16ba6b420d87a4
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-02-20 02:04:25 +0700

    Update awesome-config.plugin.zsh

--- awesome-config.plugin.zsh
+++ awesome-config.plugin.zsh
@@ -1,33 +1,5 @@
-export XDG_CONFIG_HOME="$HOME/.config"
-eval "$(/opt/homebrew/bin/brew shellenv)"
-# export STARSHIP_CONFIG="$XDG_CONFIG_HOME/starship/starship.toml"
-
-# 🚀 Load Shell Prompt
-# eval "$(starship init zsh)"
-eval "$(oh-my-posh init zsh --config $(brew --prefix oh-my-posh)/themes/bubblesextra.omp.json)"
-
-# 🐢 Atuin (Better Shell History)
-eval "$(atuin init zsh)"
-
-
 # 🐚Oh My Zsh + 🌻Zinit Turbo
 zinit depth"1" wait lucid light-mode for \
-  atinit"zicompinit; zicdreplay; unalias zi" \
-      zsh-users/zsh-syntax-highlighting \
-  atload"_zsh_autosuggest_start" \
-      zsh-users/zsh-autosuggestions \
-  blockf atpull'zinit creinstall -q .' \
-      clarketm/zsh-completions \
-  pick"themes/catppuccin_mocha-zsh-syntax-highlighting.zsh" \
-      catppuccin/zsh-syntax-highlighting \
-      jeffreytse/zsh-vi-mode \
-      aloxaf/fzf-tab \
-      hlissner/zsh-autopair \
-      fdellwing/zsh-bat \
-      phucleeuwu/yazi-zoxide.zsh \
-      phucleeuwu/aliases--.zsh \
-      phucleeuwu/catppuccin-fzf.zsh \
-      phucleeuwu/om.zsh-aliases \
       OMZP::brew \
       OMZP::sudo \
       OMZP::eza \
