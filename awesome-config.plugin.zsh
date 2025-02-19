9269d3eb4394d55c5e81795ffdfde4c9197739e2
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-02-19 12:36:39 +0700

    Update and rename README.md to awesome-config.plugin.zsh

---
+++ awesome-config.plugin.zsh
@@ -0,0 +1,16 @@
+# ────────────────────────────────────────────────────────────
+#  Plugin Configurations
+# ────────────────────────────────────────────────────────────
+
+#  `eza` (Enhanced `ls`)
+zstyle ':omz:plugins:eza' 'show-group' no  
+zstyle ':omz:plugins:eza' 'git-status' yes  
+zstyle ':omz:plugins:eza' 'icons' yes  
+
+#  `alias-finder`
+zstyle ':omz:plugins:alias-finder' autoload yes  
+zstyle ':omz:plugins:alias-finder' longer yes  
+zstyle ':omz:plugins:alias-finder' exact yes  
+zstyle ':omz:plugins:alias-finder' cheaper yes  
+
+
